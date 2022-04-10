14d4e9e0cb3dcb49490939e07854bb5aaf894f3a
HalaSaadeh <halagsaadeh@gmail.com>
2022-04-10 19:51:31 +0300

    finished get level order number

--- laf_indexing.py
+++ laf_indexing.py
@@ -1,6 +1,11 @@
 def getDendrogramHeight(nodeList, rootNode):
     """
-
+    Gets the dendrogram height (# of levels)
+    Args:
+        - nodeList: input dict of nodes
+        - rootNode: root number of input dict of nodes (highest cluster)
+    Returns:
+        - height of the dendrogram
     """
     left_child = nodeList[rootNode]["left"]
     right_child = nodeList[rootNode]["right"]
@@ -15,21 +20,48 @@ def getDendrogramHeight(nodeList, rootNode):
         return right_height + 1
 
 
-def getLevel(nodeList, rootNode, level, level_order_number =0):
+def getLevel(nodeList, rootNode, level, level_order_list):
+    """
+    Gets all the nodes in a specific level and appends them to a list
+    Args:
+        - nodeList: input dict of nodes
+        - rootNode: root number of input dict of nodes (highest cluster)
+    """
     if rootNode is None:
         return
 
     left_child = nodeList[rootNode]["left"]
     right_child = nodeList[rootNode]["right"]
     if level == 1:
-        print(rootNode, nodeList[rootNode])
+        level_order_list.append(rootNode)
     elif level > 1:
-        getLevel(nodeList, left_child, level - 1)
-        getLevel(nodeList, right_child, level - 1)
+        getLevel(nodeList, left_child, level - 1, level_order_list)
+        getLevel(nodeList, right_child, level - 1, level_order_list)
+
+
+def getLevelOrderNumbers(nodesList, rootNodeNumber):
+    """
+    Gets the level order numbers of all the ndoes
+    Args:
+        - nodeList: input dict of nodes
+        - rootNode: root number of input dict of nodes (highest cluster)
+    Returns:
+        - level_order_numbers: dict of all nodes and their level number
+    """
+
+    dendrogram_height = getDendrogramHeight(nodesList, rootNodeNumber)
+    level_order_list = []
+    for i in range(dendrogram_height):
+        getLevel(nodesList, rootNodeNumber, i, level_order_list)
+    level_order_numbers = {}
+    for i in range(len(level_order_list)):
+        level_order_numbers[level_order_list[i]] = i
+    print(level_order_numbers)
 
 
 
-def laf_indexing(nodesList, rootNodeNumber, level_order_number="0", level_number = 0, doc_id_index = {}, cluster_topic_index={}):
+def laf_indexing(nodesList, rootNodeNumber, level_order_number="0", level_number=0, doc_id_index={},
+                 cluster_topic_index={}):
     """
     Generates the cluster index (LAF numbering)
     Args:
@@ -38,6 +70,4 @@ def laf_indexing(nodesList, rootNodeNumber, level_order_number="0", level_number
     Returns:
         - cluster index table
     """
-    dendrogram_height = getDendrogramHeight(nodesList, rootNodeNumber)
-    for i in range(dendrogram_height):
-        getLevel(nodesList, rootNodeNumber, i)
+    getLevelOrderNumbers(nodesList, rootNodeNumber)
