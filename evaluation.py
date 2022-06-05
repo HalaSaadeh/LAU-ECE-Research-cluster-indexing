4ccb588249fd7fdf8f758919ea2febf1dad073c5
HalaSaadeh <halagsaadeh@gmail.com>
2022-06-05 10:56:20 +0300

    updated experiments

--- evaluation.py
+++ evaluation.py
@@ -1,23 +1,15 @@
 import timeit
 from pympler import asizeof
 from main import pre_indexing, indexing_dewey, indexing_laf
-import csv
 
-f = open('D:/Research/Implementation/undergrad-research-indexing/results.csv', 'w')
-
-# create the csv writer
-writer = csv.writer(f)
-
-csv_header = ["Dataset", "# Documents", "Dewey IndexBuild Time", "Dewey Index Build Size",
-              "LAF IndexBuild Time", "LAF Index Build Size"]
-writer.writerow(csv_header)
 
 datasets = [
     # ("Resolutions Ecosoc Eng 2021", "D:\Research\Resolutions Corpus\Res\EcoSoc\Eng 2021", 5),
-    # ("SDG Corpus", "D:/Research/SDG Corpus/", 15),
-    ("SDG Corpus Dup", "D:/Research/SDG Corpus Dup/", 30),
-    # ("Resolutions Ecosoc Eng 2020", "D:/Research/Resolutions Corpus/Res/EcoSoc/Eng 2020 Mod/", 45),
-    # ("Resolutions Escwa Since 2006", "D:/Research/Resolutions Corpus/Res/ESCWA Res (since 2006) Mod", 60),
+    ("SDG Corpus", "D:/Research/SDG Corpus/", 15),
+    # ("SDG Corpus", "D:/Research/SDG 15", 15),
+    # ("SDG Corpus Dup", "D:/Research/SDG 30", 30),
+    # ("SDG Full Dataset", "D:/Research/SDG 45",  45),
+    # ("SDG Full Dataset", "D:/Research/SDG Full Dataset 60", 60),
     # ("SDG Full Dataset", "D:/Research/SDG FullDataset - Copy/FullDataset", 120),
     # ("SDG Full Dataset", "D:/Research/SDG FullDataset Mod/FullDataset", 180),
     # ("SDG Full Dataset", "D:/Research/SDG FullDataset Mod - Copy/FullDataset", 240),
@@ -27,22 +19,19 @@ datasets = [
 
 for dataset in datasets:
 
-    nodesList, rootNumber = pre_indexing(dataset[1])
+    # nodesList, rootNumber = pre_indexing(dataset)
 
-    start = timeit.default_timer()
-    doc_id_index, cluster_topic_index = indexing_dewey(nodesList, rootNumber)
+    doc_id_index, cluster_topic_index = indexing_dewey(dataset)
     print(doc_id_index)
-    stop = timeit.default_timer()
-    dewey_build_time = stop - start
-    total_index_size_dewey = (asizeof.asizeof(doc_id_index) + asizeof.asizeof(cluster_topic_index))/(1024*1024)
 
-    start = timeit.default_timer()
-    doc_id_index, cluster_topic_index, laf_index = indexing_laf(nodesList, rootNumber)
-    stop = timeit.default_timer()
-    laf_build_time = stop - start
-    total_index_size_laf = (asizeof.asizeof(doc_id_index) + asizeof.asizeof(cluster_topic_index) +
-                            asizeof.asizeof(laf_index))/(1024*1024)
-    csv_row = [dataset[0], dataset[2], dewey_build_time, total_index_size_dewey, laf_build_time,total_index_size_laf]
-    writer.writerow(csv_row)
 
-f.close()
+    # start = timeit.default_timer()
+    # doc_id_index, cluster_topic_index, laf_index = indexing_laf(nodesList, rootNumber)
+    # stop = timeit.default_timer()
+    # laf_build_time = stop - start
+    # total_index_size_laf = (asizeof.asizeof(doc_id_index) + asizeof.asizeof(cluster_topic_index) +
+    #                         asizeof.asizeof(laf_index))/(1024*1024)
+    # csv_row = [dataset[0], dataset[2], dewey_build_time, total_index_size_dewey, laf_build_time,total_index_size_laf]
+    # writer.writerow(csv_row)
+
+# f.close()
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import timeit
+
+from pympler import asizeof
+
 from file_utils import extractDataset
 from text_utils import textToDataFrame
 from hierarchical_clustering import agglomorativeClustering
@@ -6,29 +10,55 @@ from dewey_indexing import dewey_indexing
 from sqlite_utils import insert_index_as_table
 from topic_extraction import getClusterVectors, getTopKKeywordsForEachCluster, appendKeywordListToNodeList
 from laf_indexing import laf_indexing, getLevelOrderNumbers
+import csv
+
+f = open('D:/Research/Implementation/undergrad-research-indexing/results.csv', 'w')
+
+# create the csv writer
+writer = csv.writer(f)
+
+csv_header = ["# Documents", "Doc Preprocessing Time", "Hierarchical Clustering Time",
+              "Topic Extraction Time", "Indexing Time", "Document Index Size", "Topic Index Size"]
+writer.writerow(csv_header)
 
 
 def pre_indexing(datasetPath):
+    start = timeit.default_timer()
     # Extract dataset
-    filenames, content = extractDataset(datasetPath)
+    filenames, content = extractDataset(datasetPath[1])
+    num_docs = datasetPath[2]
 
     # Convert text to tf-idf vectors
     content_df, content_featurekeys = textToDataFrame(content)
+    stop = timeit.default_timer()
+    preprocessing_time = stop - start
 
     # Compute clustering
+    start = timeit.default_timer()
     model, list_FeatureKeys = agglomorativeClustering(content_df, content_featurekeys)
 
     # Get list of cluster nodes
     nodesList, rootNodeNumber = createNodesList(content_df, model, filenames)
+    stop = timeit.default_timer()
+    clustering_time = stop - start
 
+    start = timeit.default_timer()
     # Get top K keywords from each cluster and add the information to the nodesList
     new_df = getClusterVectors(df=content_df, nodeList=nodesList, nodeListRootNumber=rootNodeNumber)
     topKkeys = getTopKKeywordsForEachCluster(new_df, 6)
     nodeList_withTopics = appendKeywordListToNodeList(nodesList, topKkeys)
-    return nodesList, rootNodeNumber
+    stop = timeit.default_timer()
+    topic_time = stop - start
+
+    results = {"num_docs": num_docs, "preprocessing": preprocessing_time, "clustering": clustering_time, "topic_extraction": topic_time}
+
+    return nodesList, rootNodeNumber, results
 
 
-def indexing_dewey(nodesList, rootNodeNumber):
+def indexing_dewey(dataset):
+    nodesList, rootNodeNumber, results = pre_indexing(dataset)
+
+    start = timeit.default_timer()
     # Generate the indexes (Dewey numbering)
     doc_id_index, cluster_topic_index = dewey_indexing(nodesList, rootNodeNumber)
 
@@ -36,6 +66,15 @@ def indexing_dewey(nodesList, rootNodeNumber):
     DEWEY_DB_PATH = "D:/Research/Implementation/undergrad-research-indexing/dewey_db.db"
     insert_index_as_table(DEWEY_DB_PATH, "doc_table_index", doc_id_index)
     insert_index_as_table(DEWEY_DB_PATH, "cluster_topic_index", cluster_topic_index)
+    stop = timeit.default_timer()
+    indexing_time = stop - start
+
+    doc_index_size = asizeof.asizeof(doc_id_index)/(1024*1024)
+    topic_index_size = asizeof.asizeof(cluster_topic_index)/(1024*1024)
+
+
+    csv_row = [results["num_docs"], results["preprocessing"], results["clustering"], results["topic_extraction"], indexing_time, doc_index_size, topic_index_size]
+    writer.writerow(csv_row)
 
     return doc_id_index, cluster_topic_index
 
