90d85337d1bf35e7e3437ce7a39fbf0dc21f9a27
HalaSaadeh <halagsaadeh@gmail.com>
2023-01-28 09:54:56 +0200

    updated clustering algorithm to genieclust

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,7 +1,7 @@
 from src.main import indexing_dewey
 
 datasets = [
-    # ("SDG Corpus", "D:/Research/SDG Corpus 10/", 10),
+    ("SDG Corpus", "D:/Research/SDG Corpus 10/", 10),
     # ("SDG Corpus", "D:/Research/SDG 15", 15),
     # ("SDG Corpus Dup", "D:/Research/SDG 30", 30),
     # ("SDG Full Dataset", "D:/Research/SDG 45",  45),
@@ -10,16 +10,16 @@ datasets = [
     # ("SDG Full Dataset", "D:/Research/SDG FullDataset Mod/FullDataset", 180),
     # ("SDG Full Dataset", "D:/Research/SDG FullDataset Mod - Copy/FullDataset", 240),
     # ("SDG Full Dataset", "D:/Research/SDG FullDataset Mod - Copy - Copy/FullDataset", 300)
-    ("SDG Full Dataset", "D:/Research/SDG/100", 100),
-    ("SDG Full Dataset", "D:/Research/SDG/200", 200),
-    ("SDG Full Dataset", "D:/Research/SDG/300", 300),
-    ("SDG Full Dataset", "D:/Research/SDG/400", 400),
-    ("SDG Full Dataset", "D:/Research/SDG/500", 500),
-    ("SDG Full Dataset", "D:/Research/SDG/600", 600),
-    ("SDG Full Dataset", "D:/Research/SDG/700", 700),
-    ("SDG Full Dataset", "D:/Research/SDG/800", 800),
-    ("SDG Full Dataset", "D:/Research/SDG/900", 900),
-    ("SDG Full Dataset", "D:/Research/SDG/1000", 1000),
+    # ("SDG Full Dataset", "D:/Research/SDG/100", 100),
+    # ("SDG Full Dataset", "D:/Research/SDG/200", 200),
+    # ("SDG Full Dataset", "D:/Research/SDG/300", 300),
+    # ("SDG Full Dataset", "D:/Research/SDG/400", 400),
+    # ("SDG Full Dataset", "D:/Research/SDG/500", 500),
+    # ("SDG Full Dataset", "D:/Research/SDG/600", 600),
+    # ("SDG Full Dataset", "D:/Research/SDG/700", 700),
+    # ("SDG Full Dataset", "D:/Research/SDG/800", 800),
+    # ("SDG Full Dataset", "D:/Research/SDG/900", 900),
+    # ("SDG Full Dataset", "D:/Research/SDG/1000", 1000),
 
 ]
 
